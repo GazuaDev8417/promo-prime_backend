b45985bb5a4b2c420d6fc8fb871afc09a88d2519
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2023-06-22 20:58:29 -0300

    manutenção do endpoint deleteFile

--- src/endpoints/deleteFile.js
+++ src/endpoints/deleteFile.js
@@ -10,18 +10,17 @@ export const deleteFile = async(req, res)=>{
         const user = await auth(req)
 
         const [contract] = await con('promo_prime_contract').where({
-            user_id: user.id
-        })
-
-        if(!contract){
-            statusCode = 403
-            throw new Error('Você não está autorizado à excluir esse registro de contrato')
-        }
-
-
-        await con('promo_prime_contract').del().where({
             id: req.params.id
         })
+
+        if(contract.user_id !== user.id){
+            statusCode = 403
+            throw new Error('Você não tem autorização para excluir esse registro de contrato')
+        }else{
+            await con('promo_prime_contract').del().where({
+                id: req.params.id
+            })
+        }        
 
 
         res.status(200).send('Registro de contrato deletado')
