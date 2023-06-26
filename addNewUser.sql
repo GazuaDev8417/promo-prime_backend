2eded4b25e5e9d1309683788f73ea830905d0df4
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2023-06-26 15:24:54 -0300

    arquivo addNewUser.sql adicionado

---
+++ addNewUser.sql
@@ -0,0 +1,5 @@
+CREATE USER 'gazua'@'localhost' IDENTIFIED BY 'alfadb';
+
+GRANT ALL PRIVILEGES ON DATABASE promo_prime TO gazua;
+GRANT ALL PRIVILEGES ON database_name.* TO 'username'@'localhost';
+FLUSH PRIVILEGES;
